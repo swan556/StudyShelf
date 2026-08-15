d25ba6906a02106174477f2b46eacd8cae210a87
Swan <swanand556@gmail.com>
2026-08-15 14:59:17 +0530

    updated pydantic model

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, HttpUrl
 from typing import List, Optional
-from datatime import datetime
+from datetime import datetime
 
 class ResourceBase(BaseModel):
     # overall base model, that field that is shared between creating and reading resources
@@ -8,8 +8,18 @@ class ResourceBase(BaseModel):
     url: HttpUrl
     description: str = ""
     category: str = "Uncategorized"
-    tags = List[str] = []
-    favourite: bool = false
+    tags: List[str] = []
+    favourite: bool = False
 
 class ResourceCreate(ResourceBase):
-    # this is what client craetes 
+    # this is what client sends when creating a resource
+    pass
+
+class Resource(ResourceBase):
+    # what the server sends back to client, also includes the generated fields
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
